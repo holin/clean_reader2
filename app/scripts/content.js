1c53401b400af0cd10b58ac6e5de836e275db174
Holin <holin.he@gmail.com>
2018-04-05 17:52:20 +0800

    Keep li style

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,53 +1,64 @@
-import $ from 'jquery';
+import $ from 'jquery'
 import _ from 'lodash'
-import Reader from './libs/reader.js';
-import hotkeys from 'hotkeys-js';
+import Reader from './libs/reader.js'
+import hotkeys from 'hotkeys-js'
 
-const reader = new Reader($);
-window.reader = reader;
-reader.msg_init_zoom_percents();
+const reader = new Reader($)
+window.reader = reader
+reader.msg_init_zoom_percents()
 // import 'chromereload/devonly'
 
-new function($) {
-  function doublePress(key, fn) {
+new function ($) {
+  function doublePress (key, fn) {
     hotkeys(key, (e) => {
-      let scope = 'scope-'+key;
+      let scope = 'scope-' + key
       if (hotkeys.getScope(scope) !== 'all') {
-        fn();
-        hotkeys.deleteScope(scope);
+        fn()
+        hotkeys.deleteScope(scope)
       } else {
-        hotkeys.setScope(scope);
+        hotkeys.setScope(scope)
         setTimeout(() => {
-          hotkeys.deleteScope(scope);
-        }, 500);
+          hotkeys.deleteScope(scope)
+        }, 500)
       }
-    });
+    })
   }
 
-  function init($) {
-
-    if (self.location.pathname == "/kbsweb/") {
-      _.each($(".schedule-item"), function(item){
-        let $item = $(item);
-        let html = _.map($item.find(".team"), function(team){
-          return $.trim($(team).text());
-        }).join("vs");
-        $item.after("<div style='margin-left:40px;'><input type='text' value='"+ html +"' style='border:1px solid #efefef; padding: 3px;text-align:center' onfocus='this.select()'></div>");
-      });
-
+  function init ($) {
+    if (window.location.pathname === '/kbsweb/') {
+      let makeTeamVSFunc = function () {
+        let inserted = false
+        _.each($('.schedule-item'), function (item) {
+          let $item = $(item)
+          let html = _.map($item.find('.team'), function (team) {
+            return $.trim($(team).text())
+          }).join('vs')
+          if (!$item.next().is('.vs-c')) {
+            $item.after("<div class='vs-c' style='margin-left:40px;'><input type='text' value='" + html + "' style='border:1px solid #efefef; padding: 3px;text-align:center' onfocus='this.select()'></div>")
+          } else {
+            inserted = true
+          }
+        })
+        if (!inserted) {
+          setTimeout(() => {
+            makeTeamVSFunc()
+          }, 1000)
+        }
+      }
+      makeTeamVSFunc()
     }
 
     // Back to top by press `b` twice in 500 millisecond.
     doublePress('b', () => {
-      $('html,body').animate({scrollTop:0},0);
-    });
+      $('html,body').animate({scrollTop: 0}, 0)
+    })
 
-    doublePress('d', () => { reader.toggle(); });
+    doublePress('d', () => { reader.toggle() })
 
     hotkeys('esc', (e) => {
-      //escape
-      reader.close();
-    });
+      // escape
+      reader.close()
+    })
   }
-  init($);
-}($);
+  init($)
+}($)
